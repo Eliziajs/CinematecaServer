d22ef8d1d1f61b43478f44684da54f550a984b6a
Eliziajs <eliziajs@gmail.com>
2023-03-16 10:20:34 -0300

    Resolvendo as classes Resouces,Repository e pom.xml

---
+++ src/main/java/br/com/appcinemateca/api/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package br.com.appcinemateca.api.repositories;
+
+import br.com.appcinemateca.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	@Query("SELECT u FROM User u WHERE u.userName =:userName")
+	User findByUsername(@Param("userName") String userName);
+}
