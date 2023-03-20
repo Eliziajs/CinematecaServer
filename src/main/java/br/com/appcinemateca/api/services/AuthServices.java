5a9262912aea79ecb67c48ce593fbbbaa90127cd
Eliziajs <eliziajs@gmail.com>
2023-03-20 17:58:43 -0300

    creation of class AuthResources

---
+++ src/main/java/br/com/appcinemateca/api/services/AuthServices.java
@@ -0,0 +1,61 @@
+package br.com.appcinemateca.api.services;
+
+import br.com.appcinemateca.api.domain.dto.security.AccountCredentialsDTO;
+import br.com.appcinemateca.api.domain.dto.security.TokenDTO;
+import br.com.appcinemateca.api.repositories.UserRepository;
+import br.com.appcinemateca.api.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServices {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity signin(AccountCredentialsDTO data) {
+		try {
+			var username = data.getUsername();
+			var password = data.getPassword();
+			authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(username, password));
+			
+			var user = repository.findByUserName(username);
+			
+			var tokenResponse = new TokenDTO();
+			if (user != null) {
+				tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+			} else {
+				throw new UsernameNotFoundException("Username " + username + " not found!");
+			}
+			return ResponseEntity.ok(tokenResponse);
+		} catch (Exception e) {
+			throw new BadCredentialsException("Invalid username/password supplied!");
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public ResponseEntity refreshToken(String username, String refreshToken) {
+		var user = repository.findByUserName(username);
+		
+		var tokenResponse = new TokenDTO();
+		if (user != null) {
+			tokenResponse = tokenProvider.refreshToken(refreshToken);
+		} else {
+			throw new UsernameNotFoundException("Username " + username + " not found!");
+		}
+		return ResponseEntity.ok(tokenResponse);
+	}
+}
--- src/main/java/br/com/appcinemateca/api/services/UserServices.java
+++ src/main/java/br/com/appcinemateca/api/services/UserServices.java
@@ -1,13 +1,13 @@
 package br.com.appcinemateca.api.services;
 
-import java.util.logging.Logger;
-
 import br.com.appcinemateca.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class UserServices implements UserDetailsService {
@@ -24,7 +24,7 @@ public class UserServices implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		logger.info("Finding one user by name " + username + "!");
-		var user = repository.findByUsername(username);
+		var user = repository.findByUserName(username);
 		if (user != null) {
 			return user;
 		} else {
