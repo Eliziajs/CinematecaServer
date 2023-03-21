c99005f1d01b792914f1b7ce4a366314eb5eac60
Eliziajs <eliziajs@gmail.com>
2023-03-21 11:51:46 -0300

    Correct and alteration into class Application and SecurityConfig

--- src/main/java/br/com/appcinemateca/Startup.java
+++ src/main/java/br/com/appcinemateca/Startup.java
@@ -4,18 +4,19 @@ package br.com.appcinemateca;
 //import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 //import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-//@SpringBootApplication
+@SpringBootApplication
 public class Startup {
 
     public static void main(String[] args) {
         SpringApplication.run(Startup.class, args);
 
 
-       /** Map<String, PasswordEncoder> encoders = new HashMap<>();
+      /** Map<String, PasswordEncoder> encoders = new HashMap<>();
         encoders.put("pbkdf2", Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5());
         PasswordEncoder passwordEncoder =
                 new DelegatingPasswordEncoder("pbkdf2", encoders);
