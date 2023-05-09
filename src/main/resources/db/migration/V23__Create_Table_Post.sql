9a658d6464f2396c863c1dc5ffbeafcd67c67b10
Eliziajs <eliziajs@gmail.com>
2023-05-09 11:59:51 -0300

    Add migration Create_Table_Post.sql, Insert_Table_Post.sql and rename of the Mensagem.class for Post.class

---
+++ src/main/resources/db/migration/V23__Create_Table_Post.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `mensagem` varchar(255) DEFAULT NULL,
+  `data` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+
+
+
+
+
+
