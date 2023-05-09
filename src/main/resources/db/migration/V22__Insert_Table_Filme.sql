16362b91c68bb02e1f0f1cf9b14e87aac5505441
Eliziajs <eliziajs@gmail.com>
2023-05-09 11:38:11 -0300

    Add migration Create_Table_Personagem.sql, Insert_Table_Personagem.sql, Create_Table_Filme.sql, Insert_Table_Filme.sql

---
+++ src/main/resources/db/migration/V22__Insert_Table_Filme.sql
@@ -0,0 +1,3 @@
+INSERT INTO `filme` VALUES (1,'Pulp Fiction',1994,'Filme violento','2023-02-01'),
+(2,'O iluminado',1994,'Filme de terror','2023-04-01'),
+(3,'Laranja mecânica',1972,'Filme filosofico e cult','2023-03-01');
