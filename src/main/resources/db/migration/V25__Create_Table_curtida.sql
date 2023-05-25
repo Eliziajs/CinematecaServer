c4289c60733086ee62e9e8838c91472791044d36
Eliziajs <eliziajs@gmail.com>
2023-05-25 19:51:45 -0300

    Add migration Create_Table_curtida.sql, Insert_Table_curtida.sql and r

---
+++ src/main/resources/db/migration/V25__Create_Table_curtida.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE IF NOT EXISTS `Curtida` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `count` bigint DEFAULT NULL,
+
+  PRIMARY KEY (`id`)
+);
+
+
+
+
+
+
+
+
---
+++ src/main/resources/db/migration/V26__Insert_Table_curtida.sql
@@ -0,0 +1,1 @@
+INSERT INTO `curtida` VALUES (1,1),(2,3),(3,1);
