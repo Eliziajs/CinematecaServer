708479f05f9e5c7f5950abab3b632d868d565f04
derio123 <deriomatheus6@gmail.com>
2023-11-23 07:44:37 -0300

    Vamos lá

--- src/main/java/br/com/appcinemateca/ApiApplication.java
+++ src/main/java/br/com/appcinemateca/ApiApplication.java
@@ -4,16 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class ApiApplication implements CommandLineRunner{
+public class ApiApplication implements CommandLineRunner {
 
-    //@Autowired
-    //private PersonRepository repository;
+    // @Autowired
+    // private PersonRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
 
-   public void run(String... args) throws Exception { }
+    public void run(String... args) throws Exception {
+    }
 }
